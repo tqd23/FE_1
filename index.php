67c8e3dc0a4422f04cd32aefe2ea83eaccfd58d1
dat <dat.tq@neo-lab.vn>
2017-09-28 14:47:55 +0700

    submit

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "urf-8">
+        <title>Front-end</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="header">
+            <div class="container">
+                <div class="header-component">
+                    <div class="logo">
+                        <h2 class="title">Aditii</h2>
+                    </div>
+                    <div class="search-component">
+                        <input type="text">
+                        <button>Search</button>
+                    </div>
+                    <div class="cart-group">
+                        <div class="cart-img">
+                            <img src="img/cart.png" alt="">
+                        </div>
+                        <div class="money">
+                            $300
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main">
+            <div class="nav-bar1">
+                <div class="container">
+                    <ul>
+                        <li><a class="item"  href="">Home</a></li>
+                        <li><a class="item">Sale</a></li>
+                        <li><a class="item">Handbags</a></li>
+                        <li><a class="item">Wallets</a></li>
+                        <li><a class="item">Accessories</a></li>
+                        <li><a class="item">Menstore</a></li>
+                        <li><a class="item">Shoes</a></li>
+                        <li><a class="item">Vintage</a></li>
+                        <li><a class="item">Services</a></li>
+                        <li><a class="item1">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="banner">
+                <div class="img-view">
+                    <img src="img/banner.jpg">
+                </div>
+            </div>
+            <div class="branded-group">
+                <div class="container">
+                    <div class="branded-item">
+                        <div class="img-view">
+                            <img src="img/shoes-2.png" alt="">
+                        </div>
+                        <div class="info-details">
+                            <div class="title">
+                                BRANDED SHOES
+                            </div>
+                            <button>
+                                SHOP
+                            </button>
+                        </div>
+                    </div>
+                    <div class="branded-item">
+                        <div class="img-view">
+                            <img src="img/shoes-4.png" alt="">
+                        </div>
+                        <div class="info-details">
+                            <div class="title">
+                                BRANDED TSHIRTS
+                            </div>
+                            <button>
+                                SHOP
+                            </button>
+                        </div>
+                    </div>
+                    <div class="branded-item">
+                        <div class="img-view">
+                            <img src="img/shoes-3.png" alt="">
+                        </div>
+                        <div class="info-details">
+                            <div class="title">
+                                BRANDED SHOES
+                            </div>
+                            <button>
+                                SHOP
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>       
+            <div class="features">
+                <p class="view-pro">
+                    FEATURED PRODUCT
+                </p>
+            </div>
+            <div class="dm">
+                <div class="container">
+                    <div class="item">
+                        <div class="view">
+                            <img src="img/dm1.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="title">BRANDED SHOES</div>						
+                            <div class="group">
+                                <div class="price">
+                                    $300
+                                </div>
+                                <button>
+                                    BUY NOW
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="view">
+                            <img src="img/dm1.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="title">BRANDED SHOES</div>						
+                            <div class="group">
+                                <div class="price">
+                                    $300
+                                </div>
+                                <button>
+                                    BUY NOW
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="view">
+                            <img src="img/dm1.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="title">BRANDED SHOES</div>						
+                            <div class="group">
+                                <div class="price">
+                                    $300
+                                </div>
+                                <button>
+                                    BUY NOW
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="view">
+                            <img src="img/dm1.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="title">BRANDED SHOES</div>						
+                            <div class="group">
+                                <div class="price">
+                                    $300
+                                </div>
+                                <button>
+                                    BUY NOW
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="view">
+                            <img src="img/dm1.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="title">BRANDED SHOES</div>						
+                            <div class="group">
+                                <div class="price">
+                                    $300
+                                </div>
+                                <button>
+                                    BUY NOW
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="view">
+                            <img src="img/dm1.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="title">BRANDED SHOES</div>						
+                            <div class="group">
+                                <div class="price">
+                                    $300
+                                </div>
+                                <button>
+                                    BUY NOW
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="linking-group">
+                <div class="container">
+                    <div class="linking-item">
+                        <div class="title">
+                            FETURED SALE
+                        </div>
+                        <div class="link-detail">Alexis Hudson</div>
+                        <div class="link-detail">American Apperel</div>
+                        <div class="link-detail">Ben Sherman</div>
+                        <div class="link-detail">Big Buddha</div>
+                        <div class="link-detail">Channel</div>
+                        <div class="link-detail">Christian Audigier</div>
+                        <div class="link-detail">Coach</div>
+                        <div class="link-detail">Cole Haan</div>
+                    </div>
+                    <div class="linking-item">
+                        <div class="title">
+                            MENS STORE
+                        </div>
+                        <div class="link-detail">Alexis Hudson</div>
+                        <div class="link-detail">American Apperel</div>
+                        <div class="link-detail">Ben Sherman</div>
+                        <div class="link-detail">Big Buddha</div>
+                        <div class="link-detail">Channel</div>
+                        <div class="link-detail">Christian Audigier</div>
+                        <div class="link-detail">Coach</div>
+                        <div class="link-detail">Cole Haan</div>
+                    </div>
+                    <div class="linking-item">
+                        <div class="title">
+                            WOMEN STORE
+                        </div>
+                        <div class="link-detail">Alexis Hudson</div>
+                        <div class="link-detail">American Apperel</div>
+                        <div class="link-detail">Ben Sherman</div>
+                        <div class="link-detail">Big Buddha</div>
+                        <div class="link-detail">Channel</div>
+                        <div class="link-detail">Christian Audigier</div>
+                        <div class="link-detail">Coach</div>
+                        <div class="link-detail">Cole Haan</div>
+                    </div>
+                    <div class="linking-item">
+                        <div class="title">
+                            QUICK LINKS
+                        </div>
+                        <div class="link-detail">Alexis Hudson</div>
+                        <div class="link-detail">American Apperel</div>
+                        <div class="link-detail">Ben Sherman</div>
+                        <div class="link-detail">Big Buddha</div>
+                        <div class="link-detail">Channel</div>
+                        <div class="link-detail">Christian Audigier</div>
+                        <div class="link-detail">Coach</div>
+                        <div class="link-detail">Cole Haan</div>
+                    </div>
+                </div>
+            </div>
+            <div class="copy-right">
+                <div class="container">
+                    Copyright &copy; 2013 Css Author
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
